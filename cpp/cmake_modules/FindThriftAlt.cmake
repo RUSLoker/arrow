b272c7baf0ef4029494b9f29de19f77b81f498cf
RUSLoker <klokotkov@ya.ru>
2026-01-13 17:39:31 +0300

    Boost::headers

--- cpp/cmake_modules/FindThriftAlt.cmake
+++ cpp/cmake_modules/FindThriftAlt.cmake
@@ -179,6 +179,8 @@ if(ThriftAlt_FOUND)
   # Workaround: thrift.pc doesn't have Boost dependency.
   if(TARGET Boost::headers)
     target_link_libraries(thrift::thrift INTERFACE Boost::headers)
+  else ()
+    message(FATAL_ERROR "NO BOOST HEADERS!!!")
   endif()
 
   if(Thrift_COMPILER_FOUND)
